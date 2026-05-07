f19771da26a543db569d57661ba88ae19cfee773
Rached-jouini <jouinirached41@gmail.com>
2026-05-07 18:03:01 +0100

    Ajout CRUD capital

---
+++ src/main/java/services/ardhi.java
@@ -0,0 +1,13 @@
+package services;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface ardhi <T> {
+
+    public void add(T t) throws SQLException;
+    public void update (T t) throws SQLException;
+    public void delete (int id) throws SQLException;
+    public List<T> select()throws SQLException;
+}
+
